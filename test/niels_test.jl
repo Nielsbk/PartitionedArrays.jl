0654894060e440aef0331266ee4b29aef6d329c2
Nielsbk <niels.keinke@student.uva.nl>
2025-04-29 12:06:52 +0200

    t

--- test/niels_test.jl
+++ test/niels_test.jl
@@ -73,11 +73,11 @@ function main(distribute)
 
     println(typeof(new_A))
     println(typeof(A))
-    @show A
-    @show new_A
-    map(new_A) do val
-        @show val
-    end
+    @show local_values(A)
+    @show local_values(new_A)
+    # map(new_A) do val
+    #     @show val
+    # end
     # if MPI.Comm_rank(comm) == 1
     #     println(typeof(new_A))
     # end
