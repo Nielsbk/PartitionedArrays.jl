3a4bcff020b06cf05f9110d36fd9be1ec9fc6cb1
Nielsbk <niels.keinke@student.uva.nl>
2025-04-29 13:48:01 +0200

    test

--- test/niels_test.jl
+++ test/niels_test.jl
@@ -61,6 +61,7 @@ function main(distribute)
     # end
 
     new_A = Adapt.adapt(CuArray,new_A)
+    @show PartitionedArrays.local_values(new_A)
     graph, V_snd_buf, V_rcv_buf, hold_data_size, snd_start_idx, change_snd, perm_snd, own_data_size, change_sparse, perm_sparse = new_cache
 
     V_snd_buf = Adapt.adapt(CuArray,V_snd_buf)
