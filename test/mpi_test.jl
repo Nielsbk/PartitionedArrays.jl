64cc4419222782750c7fabe09d5065714af04afd
Nielsbk <niels.keinke@student.uva.nl>
2025-05-06 16:15:59 +0200

    test

--- test/mpi_test.jl
+++ test/mpi_test.jl
@@ -14,8 +14,8 @@ src = mod(rank-1, size)
 
 N = 4
 
-send_mesg = Array{Float64}(undef, N)
-recv_mesg = Array{Float64}(undef, N)
+send_mesg = Array{Float64}(-1.0, N)
+recv_mesg = Array{Float64}(-1.0, N)
 
 fill!(send_mesg, Float64(rank))
 
@@ -24,12 +24,13 @@ send_mesg = CuArray(send_mesg)
 recv_mesg = CuArray(recv_mesg)
 
 
+
 rreq = MPI.Irecv!(recv_mesg, comm; source=src, tag=src+32)
 
 print("$rank: Sending   $rank -> $dst = $Array(send_mesg)\n")
 sreq = MPI.Isend(send_mesg, comm; dest=dst, tag=rank+32)
 
-stats = MPI.Waitall([rreq, sreq])
+# stats = MPI.Waitall([rreq, sreq])
 
 print("$rank: Received $src -> $rank = $Array(recv_mesg)\n")
 
