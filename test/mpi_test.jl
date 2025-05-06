5999aef331c1bd1fb001bc49163d105175c8a5aa
Nielsbk <niels.keinke@student.uva.nl>
2025-05-06 14:24:50 +0200

    mpi test

---
+++ test/mpi_test.jl
@@ -0,0 +1,34 @@
+using MPI
+using CUDA
+
+function main()
+    MPI.Init()
+    comm = MPI.COMM_WORLD
+    rank = MPI.Comm_rank(comm)
+    size = MPI.Comm_size(comm)
+
+    N = 10
+    gpu_buf = CUDA.fill(rank, N)
+
+    if rank == 0
+        all_data = CUDA.zeros(Int, N * size)
+        all_data[1:N] .= gpu_buf
+
+        for src in 1:size-1
+            offset = src * N
+            MPI.Recv!(view(all_data, offset+1:offset+N), src, 0, comm)
+        end
+
+        println("Rank 0 received:")
+        host_data = Array(reshape(all_data, N, size))
+        println(host_data)
+
+    else
+        MPI.Send!(gpu_buf, 0, 0, comm)
+    end
+
+    MPI.Barrier(comm)
+    MPI.Finalize()
+end
+
+main()
