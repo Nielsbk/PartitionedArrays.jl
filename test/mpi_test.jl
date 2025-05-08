719e981a7065b1087343872c35b1f9963582bfb5
Nielsbk <niels.keinke@student.uva.nl>
2025-05-08 12:04:59 +0200

    test from forum

--- test/mpi_test.jl
+++ test/mpi_test.jl
@@ -1,34 +1,16 @@
-# examples/04-sendrecv.jl
 using MPI
 using CUDA
-using Adapt
-
 MPI.Init()
-
 comm = MPI.COMM_WORLD
 rank = MPI.Comm_rank(comm)
 size = MPI.Comm_size(comm)
-
 dst = mod(rank+1, size)
 src = mod(rank-1, size)
-
+println("rank=$rank, size=$size, dst=$dst, src=$src")
 N = 4
-
-send_mesg = [-1,-1,-1,-1,-1,-1,-1,-1,-1,-1]
-recv_mesg = [-1,-1,-1,-1,-1,-1,-1,-1,-1,-1]
-
+send_mesg = CuArray{Float64}(undef, N)
+recv_mesg = CuArray{Float64}(undef, N)
 fill!(send_mesg, Float64(rank))
-
-send_mesg = CuArray(send_mesg)
-recv_mesg = CuArray(recv_mesg)
-
-rreq = MPI.Irecv!(recv_mesg, comm; source=src, tag=src+32)
-
-print("$rank: Sending   $rank -> $dst = $Array(send_mesg)\n")
-sreq = MPI.Isend(send_mesg, comm; dest=dst, tag=rank+32)
-
-stats = MPI.Waitall([rreq, sreq])
-
-print("$rank: Received $src -> $rank = $Array(recv_mesg)\n")
-
-MPI.Barrier(comm)
+#rreq = MPI.Irecv!(recv_mesg, src,  src+32, comm)
+MPI.Sendrecv!(send_mesg, dst, 0, recv_mesg, src, 0, comm)
+println("recv_mesg on proc $rank: $recv_mesg")
