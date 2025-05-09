9e9b0a4ce51779525fe00e5b8cbb3b222ebd8f50
Nielsbk <niels.keinke@student.uva.nl>
2025-05-09 11:45:11 +0200

    test

--- test/mpi_test.jl
+++ test/mpi_test.jl
@@ -1,5 +1,6 @@
 using MPI
 using CUDA
+
 MPI.Init()
 comm = MPI.COMM_WORLD
 rank = MPI.Comm_rank(comm)
@@ -7,10 +8,11 @@ size = MPI.Comm_size(comm)
 dst = mod(rank+1, size)
 src = mod(rank-1, size)
 println("rank=$rank, size=$size, dst=$dst, src=$src")
-N = 4
+N = 2
 send_mesg = CuArray{Float64}(undef, N)
 recv_mesg = CuArray{Float64}(undef, N)
 fill!(send_mesg, Float64(rank))
 #rreq = MPI.Irecv!(recv_mesg, src,  src+32, comm)
 MPI.Sendrecv!(send_mesg, dst, 0, recv_mesg, src, 0, comm)
+
 println("recv_mesg on proc $rank: $recv_mesg")
