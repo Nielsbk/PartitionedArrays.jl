1cf59a275d970ed028dbe937be309f70d970ebc5
Nielsbk <niels.keinke@student.uva.nl>
2025-05-27 12:37:38 +0200

    t

--- test/niels_test.jl
+++ test/niels_test.jl
@@ -46,7 +46,7 @@ function profile(distribute)
     ranks = distribute(LinearIndices((p,)))
     timing = distribute([[] for i in 1:size ])
 
-    nodes_per_dir = map(i->100000000,parts_per_dir)
+    nodes_per_dir = map(i->1000000000,parts_per_dir)
     args = PartitionedArrays.laplacian_fdm(nodes_per_dir,parts_per_dir,ranks)
 
     _,_,V,_,_ = args
