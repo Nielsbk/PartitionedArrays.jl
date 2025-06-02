e2d0a3a70086354e3b1b3563f1ad14d26b8cc8e8
Nielsbk <niels.keinke@student.uva.nl>
2025-06-02 18:21:13 +0200

    fix

--- test/niels_test.jl
+++ test/niels_test.jl
@@ -186,7 +186,7 @@ function time(distribute,n,f,nruns,type)
             t[irun] =  @elapsed PartitionedArrays.psparse_yung_sheng!(A,V,cache) |> wait
         end
         ts_in_main = PartitionedArrays.gather(map(p->t,ranks))
-        A |> centralize
+        A |> PartitionedArrays.centralize
         return ts_in_main, PartitionedArrays.gather(map(p->length(p),V)), (A.m,A.n)
     end
 
@@ -212,7 +212,7 @@ function time(distribute,n,f,nruns,type)
         t[irun] =  @elapsed PartitionedArrays.psparse_yung_sheng_gpu!(A,V,cache) |> wait
     end
     ts_in_main = PartitionedArrays.gather(map(p->t,ranks))
-    A |> centralize
+    A |> PartitionedArrays.centralize
     return ts_in_main, PartitionedArrays.gather(map(p->length(p),V)), (A.m,A.n)
 
 end
