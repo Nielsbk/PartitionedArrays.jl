f838fc7f0d62356b33bd9e140e52ce028b48a1aa
Nielsbk <niels.keinke@student.uva.nl>
2025-06-02 10:21:33 +0200

    fix

--- test/niels_test.jl
+++ test/niels_test.jl
@@ -55,6 +55,9 @@ function test_sizes(distribute)
         end
     end
     # check if it fits gpu
+    A, cache = PartitionedArrays.psparse_yung_sheng!(sparse,args...) |> fetch
+    graph, V_snd_buf, V_rcv_buf, hold_data_size, snd_start_idx, change_snd, perm_snd, own_data_size, change_sparse, perm_sparse = cache
+    
     V_snd_buf = Adapt.adapt(CuArray,V_snd_buf)
     V_rcv_buf = Adapt.adapt(CuArray,V_rcv_buf)
     perm_snd = Adapt.adapt(CuArray,perm_snd)
