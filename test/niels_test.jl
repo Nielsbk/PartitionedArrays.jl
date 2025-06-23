e49e5799ec922d61c0e114c179694f34d592ea73
Nielsbk <niels.keinke@student.uva.nl>
2025-06-24 00:32:07 +0200

    fix?

--- test/niels_test.jl
+++ test/niels_test.jl
@@ -26,7 +26,7 @@ Adapt.adapt_structure(::Type{Array}, A::CUDA.CUSPARSE.CuSparseMatrixCSC) = Spars
     collect(A.nzVal),
 )
 
-Adapt.adapt_structure(::Type{CuArray}, A::SparseMatrixCSC) = CUDA.CuSparseMatrixCSC(
+Adapt.adapt_structure(::Type{CuArray}, A::SparseMatrixCSC) = CUDA.CUSPARSE.CuSparseMatrixCSC(
     size(A)...,
     CuArray(Int64.(A.colptr)),
     CuArray(Int64.(A.rowval)),
