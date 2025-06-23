bb62901e1b2b3aaf40a3f4d9cd74a5a9c12b096b
Nielsbk <niels.keinke@student.uva.nl>
2025-06-24 00:25:23 +0200

    t

--- test/niels_test.jl
+++ test/niels_test.jl
@@ -26,6 +26,13 @@ Adapt.adapt_structure(::Type{Array}, A::CUDA.CUSPARSE.CuSparseMatrixCSC) = Spars
     collect(A.nzVal),
 )
 
+Adapt.adapt_structure(::Type{CuArray}, A::SparseMatrixCSC) = CUDA.CuSparseMatrixCSC(
+    size(A)...,
+    CuArray(Int64.(A.colptr)),
+    CuArray(Int64.(A.rowval)),
+    CuArray(A.nzval),
+)
+
 
 # function cache_to_gpu(cache)
 
