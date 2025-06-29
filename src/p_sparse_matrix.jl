b99d0764f49befbb2391ad3ccd280125b1ac4e02
Nielsbk <niels.keinke@student.uva.nl>
2025-06-30 00:21:18 +0200

    typo

--- src/p_sparse_matrix.jl
+++ src/p_sparse_matrix.jl
@@ -1606,7 +1606,7 @@ function psparse_yung_sheng_gpu!(A, V, cache)
     end
     function kernel_perm_partition!(V,perm)
         t = (blockIdx().x - 1) * blockDim().x + threadIdx().x
-        if i <= length(perm)
+        if t <= length(perm)
             i, j = perm[t]
             tmp = V[i]
             V[1] = V[j]
