33ba34d64adda1eb9d97f99f279d1bbcbfe2ea8c
Nielsbk <niels.keinke@student.uva.nl>
2025-06-29 19:23:18 +0200

    fix

--- test/niels_test.jl
+++ test/niels_test.jl
@@ -40,7 +40,7 @@ using JSON3
 # Base.size(A::CuCSCMatrix64) = (A.m, A.n)
 
 function fast_sparse_eq(A::SparseMatrixCSC, B::SparseMatrixCSC,rank)
-    diff_mask = a .!= b
+    diff_mask = A.nzval .!= B.nzval
     count_diffs = count(diff_mask)
     diff_indices = findall(A.nzval .!= B.nzval)
 
