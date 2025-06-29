1fd4926b546a4c081a4eb624249b81c529a23574
Nielsbk <niels.keinke@student.uva.nl>
2025-06-29 19:50:35 +0200

    fix?

--- src/p_sparse_matrix.jl
+++ src/p_sparse_matrix.jl
@@ -1573,7 +1573,7 @@ function psparse_yung_sheng_gpu!(A, V, cache)
         threads = 256
         if N > 0
             blocks = cld(N, threads)
-            CUDA.@cuda threads=threads blocks=blocks kernel_perm_partition!(V,perm)
+            CUDA.@sync CUDA.@cuda threads=threads blocks=blocks kernel_perm_partition!(V,perm)
         end
     end
     function sparse_matrix!(A, V, K; reset=true)
