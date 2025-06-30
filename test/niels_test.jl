092d5b6976c7a6d2388836b686e828d878d97c03
Nielsbk <niels.keinke@student.uva.nl>
2025-06-30 19:16:36 +0200

    test

--- test/niels_test.jl
+++ test/niels_test.jl
@@ -355,7 +355,11 @@ function time(distribute,n,f,nruns,type)
     # @assert PartitionedArrays.local_values(A_test) == PartitionedArrays.local_values(A_gpu)
     try 
         # CUDA.pool_status()
-        fast_sparse_eq(PartitionedArrays.centralize(A_test), PartitionedArrays.centralize(A),rank)
+        A = PartitionedArrays.centralize(A)
+        open("sparse$(size).jls", "w") do io
+            serialize(io, A)
+        end
+        fast_sparse_eq(PartitionedArrays.centralize(A_test), A,rank)
         # @test PartitionedArrays.centralize(A) == PartitionedArrays.centralize(A_test)
     catch err
         println("failed with size $(n)")
