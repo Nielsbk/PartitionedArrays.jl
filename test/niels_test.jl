85749da96dc2588961ff04fd23eeaf74e993f975
Nielsbk <niels.keinke@student.uva.nl>
2025-06-30 13:27:36 +0200

    t

--- test/niels_test.jl
+++ test/niels_test.jl
@@ -285,7 +285,7 @@ function time(distribute,n,f,nruns,type)
     ranks = distribute(LinearIndices((p,)))
     timing = distribute([[] for i in 1:size ])
 
-    nodes_per_dir = map(i->n*i,parts_per_dir)
+    nodes_per_dir = map(i->n,parts_per_dir)
     args = f(nodes_per_dir,parts_per_dir,ranks)
 
     _,_,V,_,_ = args
