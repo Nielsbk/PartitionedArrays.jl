4bf37c76a4c982d83a78c37eb6f3e0f6a6fc0f8b
Nielsbk <niels.keinke@student.uva.nl>
2025-06-30 14:07:09 +0200

    t

--- test/mpi_test.jl
+++ test/mpi_test.jl
@@ -10,14 +10,14 @@ src = mod(rank-1, size)
 println("rank=$rank, size=$size, dst=$dst, src=$src")
 N = 10
 
-send_mesg = CUDA.CuArray{Float64}(Float64(rank), N)
-recv_mesg = CUDA.CuArray{Float64}(Float64(0), N)
+send_mesg = CuArray{Float64}(undef, N)
+recv_mesg = CuArray{Float64}(undef, N)
 
-# CUDA.fill!(send_mesg, Float64(rank))
+CUDA.fill!(send_mesg, Float64(rank))
 # CUDA.fill!(recv_mesg, Float64(0))
 
 # println(Array(send_mesg))
 #rreq = MPI.Irecv!(recv_mesg, src,  src+32, comm)
 MPI.Sendrecv!(send_mesg, dst, 0, recv_mesg, src, 0, comm)
 
-println("recv_mesg on proc $rank: $(collect(recv_mesg))")
+println("recv_mesg on proc $rank: $recv_mesg)")
