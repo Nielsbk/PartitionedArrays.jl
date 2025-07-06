d74362aed4afc6d0e4fd64b8721839b267bf7e10
Nielsbk <niels.keinke@student.uva.nl>
2025-07-06 23:42:54 +0200

    t

--- test/niels_test.jl
+++ test/niels_test.jl
@@ -332,11 +332,11 @@ function test(distribute)
     
     rcv = PartitionedArrays.gather(V,destination=:all)
 
-    PartitionedArrays.map_main(rcv) do r
+    PartitionedArrays.map_main(rcv,V) do r,v
         println("map_main")
         println(typeof(r))
-        println(typeof(r))
-       @test r == V
+        println(typeof(v))
+       @test r == v
     end
 
     printonce("A_seq and A_cpu initial",rank)
