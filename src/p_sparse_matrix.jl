d94cf059bb48ab219d344b3f9e7f8fe8568ead7a
Nielsbk <niels.keinke@student.uva.nl>
2025-07-16 20:28:45 +0200

    t

--- src/p_sparse_matrix.jl
+++ src/p_sparse_matrix.jl
@@ -1608,7 +1608,7 @@ function psparse_yung_sheng_gpu!(A, V, cache)
             i = threadIdx().x + (blockIdx().x - 1) * blockDim().x
             if i <= N
                 p = perm[i]
-                CUDA.@atomic V_snd_data[p] = V_raw_snd_data[i]
+                V_snd_data[p] = V_raw_snd_data[i]
             end
             return
         end
