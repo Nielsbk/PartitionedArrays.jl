701b5d5bce12d317a3014b20d5303992c4c94e65
Nielsbk <niels.keinke@student.uva.nl>
2025-07-16 21:38:36 +0200

    t

--- src/p_sparse_matrix.jl
+++ src/p_sparse_matrix.jl
@@ -1617,13 +1617,13 @@ function psparse_yung_sheng_gpu!(A, V, cache)
             V_raw_snd_data = @view V[snd_index]
             V_snd_data = V_snd.data
             # V_snd_data[perm] .= V_raw_snd_data
-            CUDA.@sync blocking=true V_snd_data[perm] .= V_raw_snd_data
-            # N = length(perm)
-            # threads = 256
-            # blocks = cld(N, threads)
-            # if N > 0
-            #     CUDA.@cuda threads=threads blocks=blocks scatter_kernel!(V_snd_data, perm, V_raw_snd_data,N)
-            # end
+            # CUDA.@sync blocking=true V_snd_data[perm] .= V_raw_snd_data
+            N = length(perm)
+            threads = 256
+            blocks = cld(N, threads)
+            if N > 0
+                CUDA.@cuda threads=threads blocks=blocks scatter_kernel!(V_snd_data, perm, V_raw_snd_data,N)
+            end
         
     end
 
